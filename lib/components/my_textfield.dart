048f769c922fb79a86af2ad54bebe28e8333d97f
flack <163582560+FlackCode@users.noreply.github.com>
2024-07-06 19:53:55 +0300

    Finished up Login Page

---
+++ lib/components/my_textfield.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MyTextfield extends StatelessWidget {
+  final String hintText;
+  final bool? obscureText;
+  final TextEditingController controller;
+  const MyTextfield({super.key, required this.hintText, this.obscureText, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: TextField(
+        obscureText: obscureText ?? false,
+        controller: controller,
+        decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+                borderSide:
+                    BorderSide(color: Theme.of(context).colorScheme.tertiary)),
+            focusedBorder: OutlineInputBorder(
+                borderSide:
+                    BorderSide(color: Theme.of(context).colorScheme.primary)),
+            fillColor: Theme.of(context).colorScheme.secondary,
+            filled: true,
+            hintText: hintText,
+            hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary)),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:chatapp/pages/login_page.dart';
+import 'package:chatapp/services/consts.dart';
+import 'package:chatapp/themes/light_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: FIREBASE_API_KEY,
+            appId: FIREBASE_APP_ID,
+            messagingSenderId: FIREBASE_MESSAGING_ID,
+            projectId: FIREBASE_PROJECT_ID));
+  } catch (e) {
+    print(e.toString());
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+      theme: lightMode,
+    );
+  }
+}
